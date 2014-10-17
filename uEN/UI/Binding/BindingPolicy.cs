818460c0eec25e1e77961b736ff7b3d50c51ce5a
Shinichi Ueno <uen.agile@gmail.com>
2014-10-17 17:55:41 +0900

    ポリシーの追加

---
+++ uEN/UI/Binding/BindingPolicy.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace uEN.UI.Binding
+{
+
+    public class BindingPolicy
+    {
+        private static readonly bool validatesOnDataErrors =
+            BizUtils.AppSettings("BindingPolicy.ValidatesOnDataErrors", true);
+        private static readonly bool validatesOnExceptions =
+            BizUtils.AppSettings("BindingPolicy.ValidatesOnExceptions", true);
+        private static readonly bool validatesOnNotifyDataErrors =
+            BizUtils.AppSettings("BindingPolicy.ValidatesOnNotifyDataErrors", true);
+
+        public BindingPolicy()
+        {
+            ValidatesOnDataErrors = validatesOnDataErrors;
+            ValidatesOnExceptions = validatesOnExceptions;
+            ValidatesOnNotifyDataErrors = validatesOnNotifyDataErrors;
+            BindingMode = System.Windows.Data.BindingMode.Default;
+        }
+
+        public string StringFormat { get; set; }
+        public BindingMode BindingMode { get; set; }
+        public UpdateSourceTrigger? UpdateSourceTrigger { get; set; }
+        public bool ValidatesOnDataErrors { get; set; }
+        public bool ValidatesOnExceptions { get; set; }
+        public bool ValidatesOnNotifyDataErrors { get; set; }
+        public IValueConverter Converter { get; set; }
+    }
+}
