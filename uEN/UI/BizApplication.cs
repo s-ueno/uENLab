27df456cbfcc06b2aa5a3b2278a0d4ecd758ee0d
Shinichi Ueno <uen.agile@gmail.com>
2016-02-11 17:29:49 +0900

    調整

--- uEN/UI/BizApplication.cs
+++ uEN/UI/BizApplication.cs
@@ -13,6 +13,13 @@ namespace uEN.UI
 {
     public class BizApplication : Application
     {
+        public bool IsShuttingDown { get; private set; }
+        protected override void OnExit(ExitEventArgs e)
+        {
+            this.IsShuttingDown = true;
+            base.OnExit(e);
+        }
+
         public ThemeManager ThemeManager
         {
             get { return Singleton<ThemeManager>.Value; }
@@ -21,6 +28,8 @@ namespace uEN.UI
 
         public void Start(BizViewModel mainViewModel, AppStyle style = AppStyle.Modern, AppTheme theme = AppTheme.Light)
         {
+            if (IsShuttingDown) return;
+
             ShutdownMode = System.Windows.ShutdownMode.OnMainWindowClose;
 
             ThemeManager.Style = style;
@@ -35,14 +44,63 @@ namespace uEN.UI
             mainViewModel.IsWindowContent = true;
 
             Initialize(window, mainViewModel);
+
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+            this.DispatcherUnhandledException += BizApplication_DispatcherUnhandledException;
+
             Run(window);
         }
-
-       
-
         protected virtual void Initialize(Window mainWindow, BizViewModel mainViewModel)
         {
 
+        }
+
+        void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            var ex = (Exception)e.ExceptionObject;
+            try
+            {
+                OnUnhandledExceptionRaw(sender, ex);
+            }
+            catch
+            {
+            }
+        }
+        void BizApplication_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            try
+            {
+                OnUnhandledExceptionRaw(sender, e.Exception);
+            }
+            catch
+            {
+            }
+            finally
+            {
+                e.Handled = true;
+            }
+        }
+        void OnUnhandledExceptionRaw(object sender, Exception ex)
+        {
+            if (engaged) return;
+
+            engaged = true;
+            try
+            {
+                OnUnhandledException(sender, ex);
+            }
+            finally
+            {
+                engaged = false;
+            }
+        }
+        static volatile bool engaged = false;
+
+        protected virtual void OnUnhandledException(object sender, Exception ex)
+        {
+            var exPolicy = Repository.GetPriorityExport<uEN.UI.DataBinding.IExceptionPolicy>();
+            if (exPolicy != null)
+                exPolicy.Do(sender, ex);
         }
     }
 }
